36b1392d6983f6e68eb7fb0f04ba63b8cc5dbd99
jinyuxin <15855528782@163.com>
2019-09-16 17:17:13 +0800

    implement linked list merged with three ways

---
+++ java/core-module/src/main/java/xyz/jinyuxin/offer/Solution25.java
@@ -0,0 +1,158 @@
+package xyz.jinyuxin.offer;
+
+/**
+ * 面试题25：合并两个有序的链表
+ */
+public class Solution25 {
+  public static void main(String[] args) {
+    int[] arr1 = {1, 3, 5, 7, 9, 10};
+    int[] arr2 = {2, 4, 6, 8};
+    ListNode list1 = ListNode.createListTail(arr1);
+    ListNode list2 = ListNode.createListTail(arr2);
+    System.out.println("递归方法合并结果为：");
+    ListNode.printList(mergeList(list1.next, list2.next));
+
+    ListNode list3 = ListNode.createListTail(arr1);
+    ListNode list4 = ListNode.createListTail(arr2);
+    System.out.println("利用新链表的非递归方法合并结果为：");
+    ListNode.printList(mergeList1(list3.next, list4.next));
+
+    ListNode list5 = ListNode.createListTail(arr1);
+    ListNode list6 = ListNode.createListTail(arr2);
+    System.out.println("不利用新链表的非递归方法合并结果为：");
+    ListNode.printList(mergeList2(list5.next, list6.next));
+  }
+
+  /**
+   * 递归方式，不用新的链表。
+   *
+   * @param list1 不带头结点的链表1
+   * @param list2 不带头结点的链表2
+   * @return 不带头结点的新链表
+   */
+
+  public static ListNode mergeList(ListNode list1, ListNode list2) {
+    //如果链表1为空，直接返回链表2
+    if (list1 == null) {
+      return list2;
+    }
+
+    //如果链表2为空，直接返回链表1
+    if (list2 == null) {
+      return list1;
+    }
+
+    ListNode newList;
+    if (list1.data < list2.data) {
+      newList = list1;
+      newList.next = mergeList(list1.next, list2);
+    } else {
+      newList = list2;
+      newList.next = mergeList(list1, list2.next);
+    }
+
+    return newList;
+  }
+
+  /**
+   * 非递归方式，用一个新链表返回。
+   *
+   * @param list1 不带头结点的链表1
+   * @param list2 不带头结点的链表2
+   * @return 不带头结点的新链表
+   */
+  public static ListNode mergeList1(ListNode list1, ListNode list2) {
+    //如果链表1为空，直接返回链表2
+    if (list1 == null) {
+      return list2;
+    }
+
+    //如果链表2为空，直接返回链表1
+    if (list2 == null) {
+      return list1;
+    }
+
+    //newList始终指向新链表的头结点
+    ListNode newList = new ListNode();
+    //curr作为新链表的遍历指针
+    ListNode curr = newList;
+    ListNode next;
+    while (list1 != null && list2 != null) {
+      if (list1.data <= list2.data) {
+        next = new ListNode(list1.data);
+        curr.next = next;
+        curr = curr.next;
+        list1 = list1.next;
+      } else {
+        next = new ListNode(list2.data);
+        curr.next = next;
+        curr = curr.next;
+        list2 = list2.next;
+      }
+    }
+
+    if (list1 != null) {
+      curr.next = list1;
+    }
+
+    if (list2 != null) {
+      curr.next = list2;
+    }
+
+    return newList.next;
+  }
+
+  /**
+   * 非递归方式，用一个新链表返回。
+   *
+   * @param list1 不带头结点的链表1
+   * @param list2 不带头结点的链表2
+   * @return 不带头结点的新链表
+   */
+  public static ListNode mergeList2(ListNode list1, ListNode list2) {
+    //如果链表1为空，直接返回链表2
+    if (list1 == null) {
+      return list2;
+    }
+
+    //如果链表2为空，直接返回链表1
+    if (list2 == null) {
+      return list1;
+    }
+
+    //head始终指向链表头结点
+    ListNode head = null;
+    //head指向首元素小的链表
+    if (list1.data <= list2.data) {
+      head = list1;
+      list1 = list1.next;
+    } else {
+      head = list2;
+      list2 = list2.next;
+    }
+
+    //作为比较指针
+    ListNode curr = head;
+    while (list1 != null && list2 != null) {
+      if (list1.data <= list2.data) {
+        curr.next = list1;
+        curr = curr.next;
+        list1 = list1.next;
+      } else {
+        curr.next = list2;
+        curr = curr.next;
+        list2 = list2.next;
+      }
+    }
+
+    if (list1 != null) {
+      curr.next = list1;
+    }
+
+    if (list2 != null) {
+      curr.next = list2;
+    }
+
+    return head;
+  }
+}
