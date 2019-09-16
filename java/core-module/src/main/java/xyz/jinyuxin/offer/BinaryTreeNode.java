bc344268e28762966358b14d6046c3543573d906
jinyuxin <15855528782@163.com>
2019-09-16 13:47:12 +0800

    add a separated binary tree definition

---
+++ java/core-module/src/main/java/xyz/jinyuxin/offer/BinaryTreeNode.java
@@ -0,0 +1,12 @@
+package xyz.jinyuxin.offer;
+
+public class BinaryTreeNode {
+  int data;
+  BinaryTreeNode left, right;
+
+  public BinaryTreeNode(int data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
